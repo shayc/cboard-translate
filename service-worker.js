938ad3cab72774eb3475f79a045acd0ce40e11e4
Shay Cojocaru <shayc@outlook.com>
2017-02-15 02:02:21 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","a07da08bae62e556cc673bf53db51fe2"],["manifest.json","df33d49787ee6b11c0b3390acd8b7daa"],["static/css/main.53599f48.css","5f1c842b9afa700ab0cebe9be3119d42"],["static/js/main.740da9a0.js","1f6ebccf04bd58c5b7a276f26dfd3095"]];
+var precacheConfig = [["index.html","761f68882a25ae6040cffbbaf33c97a6"],["manifest.json","df33d49787ee6b11c0b3390acd8b7daa"],["static/css/main.53599f48.css","5f1c842b9afa700ab0cebe9be3119d42"],["static/js/main.65180184.js","eb00af71b86b647526fdb23a82b25ab0"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
